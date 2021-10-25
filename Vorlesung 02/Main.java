39439d6f60487be5e302e6019f6af63d6704e5b3
QuexerDev <elijafanke@gmail.com>
2021-10-25 09:07:53 +0200

    Main. java aus der 2. Vorlesung initial

---
+++ Vorlesung 02/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        System.out.println((a+b) + " « ist meine Zahl!");
+
+        boolean ba = true;
+        boolean bb = false;
+
+        boolean bc = ba | bb;
+        
+        boolean bd = ba || bb;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
